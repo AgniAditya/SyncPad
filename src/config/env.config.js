5019cd3e706e10131b55b2d93f4a7b4c069832eb
AgniAditya <agniaditya05@gmail.com>
2025-08-15 15:01:37 +0530

    adding more env variables

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -7,9 +7,17 @@ dotenv.config({
 const PORT = process.env.PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 const DB_NAME = process.env.DB_NAME;
+const ACCESS_TOKEN_SECERT = process.env.ACCESS_TOKEN_SECERT;
+const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+const REFRESH_TOKEN_SECERT = process.env.REFRESH_TOKEN_SECERT;
+const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
 
 export {
     PORT,
     MONGODB_URI,
-    DB_NAME
+    DB_NAME,
+    ACCESS_TOKEN_EXPIRY,
+    ACCESS_TOKEN_SECERT,
+    REFRESH_TOKEN_SECERT,
+    REFRESH_TOKEN_EXPIRY
 }
