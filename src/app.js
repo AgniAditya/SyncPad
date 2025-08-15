91e4f198c0b7e57bd6f04683bf5ec1f5cb8e0bc0
AgniAditya <agniaditya05@gmail.com>
2025-08-15 16:01:44 +0530

    add: initialize express app with CORS and cookie parser middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import express from 'express'
+import cors from 'cors'
+import cookieParser from 'cookie-parser'
+import {CORS_ORIGIN} from './config/env.config.js'
 
 const app = express()
+
+app.use(cors({
+    origin : CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit : '20kb'}))
+app.use(express.urlencoded({extended:true,limit:'20kb'}))
+app.use(cookieParser())
 
 export {app}
