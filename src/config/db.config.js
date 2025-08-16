b006256325d364655c4702d042efc72ffe657b5d
AgniAditya <agniaditya05@gmail.com>
2025-08-16 20:38:06 +0530

    refactor: rename PORT to HTTP_PORT and add SOCKET_PORT in env configuration

--- src/config/db.config.js
+++ src/config/db.config.js
@@ -4,7 +4,7 @@ import {DB_NAME , MONGODB_URI} from './env.config.js'
 const ConnectDB = async () => {
     try {
         const connectionResponse = await mongoose.connect(`${MONGODB_URI}/${DB_NAME}`)
-        console.log(`\n MongoDB connected !! DB Host : ${connectionResponse.connection.host}`)
+        console.log(`MongoDB connected !! DB Host : ${connectionResponse.connection.host}`)
     } catch (error) {
         console.log("MONGODB connection FAILED:",error);
         process.exit(1)
--- src/config/env.config.js
+++ src/config/env.config.js
@@ -4,7 +4,7 @@ dotenv.config({
     path : './env'
 })
 
-const PORT = process.env.PORT || 3000;
+const HTTP_PORT = process.env.HTTP_PORT || 3000;
 const MONGODB_URI = process.env.MONGODB_URI;
 const DB_NAME = process.env.DB_NAME;
 const ACCESS_TOKEN_SECERT = process.env.ACCESS_TOKEN_SECERT;
@@ -12,14 +12,16 @@ const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
 const REFRESH_TOKEN_SECERT = process.env.REFRESH_TOKEN_SECERT;
 const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
 const CORS_ORIGIN = process.env.CORS_ORIGIN;
+const SOCKET_PORT = process.env.SOCKET_PORT;
 
 export {
-    PORT,
+    HTTP_PORT,
     MONGODB_URI,
     DB_NAME,
     ACCESS_TOKEN_EXPIRY,
     ACCESS_TOKEN_SECERT,
     REFRESH_TOKEN_SECERT,
     REFRESH_TOKEN_EXPIRY,
-    CORS_ORIGIN
+    CORS_ORIGIN,
+    SOCKET_PORT
 }
