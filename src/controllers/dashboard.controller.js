2c90f126fddc7ac18a906a3ffb8b3cadc3342754
AgniAditya <agniaditya05@gmail.com>
2025-08-17 21:05:22 +0530

    fix: update getAllWorkSpacesOfUser call to remove select method for workspace retrieval

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,13 +1,14 @@
 import { ApiError } from "../utils/apirError.js";
 import { ApiResponse } from "../utils/apiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { getAllWorkSpacesOfUser } from "../services/dashboard.service.js";
 
 const getDashboardInfo = asyncHandler( async (req,res) => {
     try {
         const userId = req.user?._id
         if(!userId) throw new ApiError(404,"user id not found");
         
-        const workspaces = await getAllWorkSpacesOfUser(userId).select("-document -members");
+        const workspaces = await getAllWorkSpacesOfUser(userId)
         if(!workspaces) throw new ApiError(404,"invalid user id");
     
         return res.status(200)
