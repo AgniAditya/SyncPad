a9a7821b2fc7a4ecf0bd7a5baca530088ec16faa
AgniAditya <agniaditya05@gmail.com>
2025-08-17 17:53:20 +0530

    fix: correct import path for ApiError in dashboard.service.js and add newline at end of file

--- src/services/dashboard.service.js
+++ src/services/dashboard.service.js
@@ -1,9 +1,13 @@
 import { Workspace } from "../models/workspace.model.js"
-import { ApiError } from "../utils/apirError"
+import { ApiError } from "../utils/apirError.js"
 
 const getAllWorkSpacesOfUser = async (adminId) => {
     const workspaces = await Workspace.find({admin : adminId})
     if(!workspaces) throw new ApiError(404,"invalid admin id");
 
     return workspaces;
+}
+
+export {
+    getAllWorkSpacesOfUser
 }
